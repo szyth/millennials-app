7b9b7e4ab6a7774f3c627bf81ac971b997101e3b
shayannasir <sshayann7@gmail.com>
2019-10-27 04:34:04 +0530

    View all contacts

---
+++ app/src/main/java/com/shayannasir/millennials_app/FindUserActivity.java
@@ -0,0 +1,55 @@
+package com.shayannasir.millennials_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+public class FindUserActivity extends AppCompatActivity {
+
+    private RecyclerView mUserList;
+    private RecyclerView.Adapter mUserListAdapter;
+    private RecyclerView.LayoutManager mUserListLayoutManager;
+
+    ArrayList<UserObject> userList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_user);
+
+        userList = new ArrayList<>();
+
+        initializeRecyclerView();
+        getContactList();
+
+    }
+
+    private void getContactList(){
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+        while(phones.moveToNext()){
+            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phone = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            UserObject mContacts = new UserObject(name, phone);
+            userList.add(mContacts);
+            mUserListAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private void initializeRecyclerView() {
+        mUserList  = findViewById(R.id.userlist);
+        mUserList.setNestedScrollingEnabled(false);
+        mUserList.setHasFixedSize(false);
+        mUserListLayoutManager = new LinearLayoutManager(getApplicationContext(), RecyclerView.VERTICAL, false);
+        mUserList.setLayoutManager(mUserListLayoutManager);
+        mUserListAdapter = new UserListAdapter(userList);
+        mUserList.setAdapter(mUserListAdapter);
+    }
+}
--- app/src/main/java/com/shayannasir/millennials_app/MainPageActivity.java
+++ app/src/main/java/com/shayannasir/millennials_app/MainPageActivity.java
@@ -2,7 +2,9 @@ package com.shayannasir.millennials_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +19,14 @@ public class MainPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_page);
 
         Button mLogout = findViewById(R.id.logout);
+        Button mFindUser = findViewById(R.id.findUser);
+        mFindUser.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), FindUserActivity.class));
+            }
+        });
         mLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,5 +38,14 @@ public class MainPageActivity extends AppCompatActivity {
                 return;
             }
         });
+
+        getPermissions();
+    }
+
+    private void getPermissions() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS}, 1);
+        }
+
     }
 }
